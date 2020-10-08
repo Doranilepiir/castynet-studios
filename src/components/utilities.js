a4be37ee63dcfb0ca2b9861323cfccf0dbea445c
castynet <riungemaina@gmail.com>
2020-10-08 16:02:35 +0300

    components/utilities: created & added projectsData

---
+++ src/components/utilities.js
@@ -0,0 +1,23 @@
+import digikidsImg from "../static/digikids_logo.png";
+import gadgetbuffsImg from "../static/gadgetbuffs_logo.png";
+import genztechImg from "../static/genztech_logo.png";
+
+export const ProjectsData = {
+    items: [{
+        id: 1,
+        imgLink: digikidsImg,
+        description: "Digikids Logo",
+
+    }, {
+        id: 2,
+        imgLink: gadgetbuffsImg,
+        description: "Gadgetbuffs Logo",
+
+    }, {
+        id: 3,
+        imgLink: genztechImg,
+        description: "Genztech Logo",
+
+    }]
+
+};
