12ef7b0bea2e0571556afcadc7aceb8d35fc6ae7
castynet <riungemaina@gmail.com>
2020-10-14 20:58:18 +0300

    components/footer: styled

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -8,26 +8,59 @@ export default function Footer() {
 
   return (
     <>
-      <p>&#169; 2018 - 2020 Castynet&#8482; Studios - a Software Engine</p>
-      <IconsWrap>
-        <a href={Links.Github} target={blank} rel={external}>
-          <Icons.Github />
-        </a>
-        <a href={Links.Behance} target={blank} rel={external}>
-          <Icons.Behance />
-        </a>
-        <a href={Links.Twitter} target={blank} rel={external}>
-          <Icons.Twitter />
-        </a>
-        <a href={Links.Pinterest} target={blank} rel={external}>
-          <Icons.Pinterest />
-        </a>
-        <a href={Links.Instagram} target={blank} rel={external}>
-          <Icons.Instagram />
-        </a>
-      </IconsWrap>
+      <Wrapper>
+        <p>&#169; 2018 - 2020 Castynet Studios - Empowering Software</p>
+        <IconsWrap>
+          <a href={Links.Github} target={blank} rel={external}>
+            <Icons.Github size="25" />
+          </a>
+          <a href={Links.Behance} target={blank} rel={external}>
+            <Icons.Behance size="25" />
+          </a>
+          <a href={Links.Twitter} target={blank} rel={external}>
+            <Icons.Twitter size="25" />
+          </a>
+          <a href={Links.Pinterest} target={blank} rel={external}>
+            <Icons.Pinterest size="25" />
+          </a>
+          <a href={Links.Instagram} target={blank} rel={external}>
+            <Icons.Instagram size="25" />
+          </a>
+        </IconsWrap>
+      </Wrapper>
     </>
   );
 }
 
-const IconsWrap = styled.div``;
+const Wrapper = styled.div`
+  background-color: rgb(33 150 243 / 0.1);
+  padding: 25px 0;
+
+  p {
+    color: #000033;
+    font-weight: 600;
+    text-align: center;
+    text-transform: lowercase;
+    width: 100%;
+  }
+`;
+
+const IconsWrap = styled.div`
+  display: grid;
+  grid-auto-flow: column;
+  justify-content: space-between;
+  align-content: center;
+  gap: 15px;
+  max-width: 200px;
+  margin: 15px auto 0 auto;
+  border-top: 1px solid #ff5722;
+  padding: 5px 20px 0 20px;
+
+  a {
+    color: #ff0057;
+
+    &:hover {
+      color: #0176d3;
+    }
+  }
+`;
