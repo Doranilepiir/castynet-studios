f87c3fd7af16e500397b1bd952c66911c7ba12ab
castynet <riungemaina@gmail.com>
2020-10-14 13:20:34 +0300

    components/utilities: imported & exported icons, exported links

--- src/components/utilities.js
+++ src/components/utilities.js
@@ -1,3 +1,10 @@
+import { Github } from "@styled-icons/boxicons-logos/Github";
+import { Behance } from "@styled-icons/boxicons-logos/Behance";
+import { Twitter } from "@styled-icons/boxicons-logos/Twitter";
+import { PinterestAlt } from "@styled-icons/boxicons-logos/PinterestAlt";
+import { Instagram } from "@styled-icons/boxicons-logos/Instagram";
+import { LinkOutline } from "@styled-icons/evaicons-outline/LinkOutline";
+
 export const ProjectsData = {
     items: [{
         id: 1,
@@ -20,3 +27,27 @@ export const ProjectsData = {
     }]
 
 };
+
+export const Icons = {
+    Github: Github,
+    Behance: Behance,
+    Twitter: Twitter,
+    Pinterest: PinterestAlt,
+    Instagram: Instagram,
+    Link: LinkOutline,
+}
+
+export const Links = {
+
+    Github: "https://go.genztech.xyz/Github",
+
+    Behance: "https://go.genztech.xyz/Behance",
+
+    Twitter: "https://go.genztech.xyz/twitter",
+
+    Pinterest: "https://go.genztech.xyz/pintrest",
+
+    Instagram: "https://go.genztech.xyz/instagram",
+
+    GoogleListing: "https://go.genztech.xyz/google_listing",
+}
