e942f37857776230561c159ec0ba660c8d5b0f0d
castynet <riungemaina@gmail.com>
2020-12-20 19:13:13 +0300

    components/utilities: added images

--- src/components/utilities.js
+++ src/components/utilities.js
@@ -3,70 +3,78 @@ import { Behance } from "@styled-icons/boxicons-logos/Behance";
 import { Twitter } from "@styled-icons/boxicons-logos/Twitter";
 import { PinterestAlt } from "@styled-icons/boxicons-logos/PinterestAlt";
 import { Instagram } from "@styled-icons/boxicons-logos/Instagram";
-import { LinkOutline } from "@styled-icons/evaicons-outline/LinkOutline";
+import { Link } from "@styled-icons/evil/Link";
 import { ExpandMore } from "@styled-icons/material-rounded/ExpandMore";
 import { ExpandLess } from "@styled-icons/material-rounded/ExpandLess";
 import { HorizontalRule } from "@styled-icons/material-outlined/HorizontalRule";
 import { BadgeCheck } from "@styled-icons/boxicons-regular/BadgeCheck";
 import { Location } from "@styled-icons/ionicons-sharp/Location";
-import { Connectdevelop} from "@styled-icons/fa-brands/Connectdevelop";
+import { Connectdevelop } from "@styled-icons/fa-brands/Connectdevelop";
 import { GearWideConnected } from "@styled-icons/bootstrap/GearWideConnected";
 import { SuitHeart } from "@styled-icons/bootstrap/SuitHeart";
 import { CloudLightning } from "@styled-icons/boxicons-regular/CloudLightning";
 
-
 export const ProjectsData = {
-    items: [{
-        id: 1,
-        name: "Digikids",
-        type: "E-Learning Portal",
-        link: "/",
-
-    }, {
-        id: 2,
-        name: "GadgetBuffs",
-        type: "Brand Website",
-        link: "/",
-
-    }, {
-        id: 3,
-        name: "Genztech",
-        type: "In-house Brand",
-        link: "/",
-
-    }]
-
+  items: [
+    {
+      id: 1,
+      name: "Digikids",
+      type: "E-Learning Portal",
+      link: "https://elearning.digikids.co.ke/",
+      image: "../static/digikids_logo.png",
+    },
+    {
+      id: 2,
+      name: "GadgetBuffs",
+      type: "Brand Website",
+      link: "https://gadgetbuffs.com/",
+      image: "../static/gadgetbuffs_logo.png",
+    },
+    {
+      id: 3,
+      name: "Genztech",
+      type: "In-house Brand",
+      link: "https://genztech.xyz/",
+      image: "../static/genztech_logo.png",
+    },
+    {
+      id: 3,
+      name: "shop.genztech.xyz",
+      type: "In-house Brand",
+      link: "/",
+      image: "../static/shop.genztech_logo.png",
+    },
+  ],
 };
 
 export const Icons = {
-    Github: Github,
-    Behance: Behance,
-    Twitter: Twitter,
-    Pinterest: PinterestAlt,
-    Instagram: Instagram,
-    Link: LinkOutline,
-    Expand: ExpandMore,
-    Dash: HorizontalRule,
-    Check: BadgeCheck,
-    Location: Location,
-    Connect: Connectdevelop,    
-    Gear: GearWideConnected,
-    Heart: SuitHeart,
-    Cloud: CloudLightning,
-    ExpandLess: ExpandLess,
-}
+  Github: Github,
+  Behance: Behance,
+  Twitter: Twitter,
+  Pinterest: PinterestAlt,
+  Instagram: Instagram,
+  Link: Link,
+  Expand: ExpandMore,
+  Dash: HorizontalRule,
+  Check: BadgeCheck,
+  Location: Location,
+  Connect: Connectdevelop,
+  Gear: GearWideConnected,
+  Heart: SuitHeart,
+  Cloud: CloudLightning,
+  ExpandLess: ExpandLess,
+};
 
 export const Links = {
+  Github: "https://go.genztech.xyz/Github",
 
-    Github: "https://go.genztech.xyz/Github",
+  Behance: "https://go.genztech.xyz/Behance",
 
-    Behance: "https://go.genztech.xyz/Behance",
+  Twitter: "https://go.genztech.xyz/twitter",
 
-    Twitter: "https://go.genztech.xyz/twitter",
+  Pinterest: "https://go.genztech.xyz/pintrest",
 
-    Pinterest: "https://go.genztech.xyz/pintrest",
+  Instagram: "https://go.genztech.xyz/instagram",
 
-    Instagram: "https://go.genztech.xyz/instagram",
-
-    GoogleListing: "https://go.genztech.xyz/google_listing",
-}
+  GoogleListing: "https://go.genztech.xyz/google_listing",
+};
