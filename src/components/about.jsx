ee9090f8976e465c13949ca1fee93c89d115531a
castynet <riungemaina@gmail.com>
2021-06-21 18:45:11 +0300

    components/about: prettier cleanup & changed hover color to pink

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,68 +1,45 @@
-import React from "react";
-import styled from "styled-components";
-import { Icons, Links } from "./utilities";
+import React from "react"
+import styled from "styled-components"
+import { Icons, Links } from "./utilities"
 
 export default function About() {
-  const external = "noopener noreferrer external";
-  const blank = "_blank";
+  const external = "noopener noreferrer external"
+  const blank = "_blank"
 
   return (
     <>
       <Wrapper>
         <AboutDescription>
-          Castynet is a small team of{" "}
-          <span className="marked">Software Developers</span> based in{" "}
-          <span className="marked">Nairobi KE</span>, We are high spirited geeks
-          passionate about tech.
+          Castynet is a small team of <span className="marked">Software Developers</span> based in{" "}
+          <span className="marked">Nairobi KE</span>, We are high spirited geeks passionate about tech.
           <br />
-          Our mission is to create{" "}
-          <span className="marked">Empowering Software</span> across the{" "}
-          <span className="marked">Web</span>. We are powered by coffee, unreal
-          imagination and attention to detail. <br />
+          Our mission is to create <span className="marked">Empowering Software</span> across the{" "}
+          <span className="marked">Web</span>. We are powered by coffee, unreal imagination and attention
+          to detail. <br />
           If you have a project you think would be a fit for us do{" "}
           <span className="marked">reach out</span>.
         </AboutDescription>
         <Rating>
           <ListTitle>appraised at</ListTitle>
-          <a
-            href={Links.GoogleListing}
-            target={blank}
-            rel={external}
-            aria-label="Rating on Google"
-          >
+          <a href={Links.GoogleListing} target={blank} rel={external} aria-label="Rating on Google">
             <li>
-              5/5 Star on Google <Icons.Link size="20" />
+              5/5 Star on Google <Icons.Link size="17" />
             </li>
           </a>
         </Rating>
         <Stats>
           <ListTitle>statistik-ally</ListTitle>
-          <a
-            href={Links.Github}
-            target={blank}
-            rel={external}
-            aria-label="Github"
-          >
+          <a href={Links.Github} target={blank} rel={external} aria-label="Github">
             <li>
               847,030 lines of code <Icons.Github size="20" />
             </li>
           </a>
-          <a
-            href={Links.Instagram}
-            target={blank}
-            rel={external}
-            aria-label="Instagram"
-          >
+          <a href={Links.Instagram} target={blank} rel={external} aria-label="Instagram">
             <li>
               1.8k Peers <Icons.Instagram size="20" />
             </li>
           </a>
-          <a
-            href={Links.Twitter}
-            target={blank}
-            rel={external}
-            aria-label="Twitter"
-          >
+          <a href={Links.Twitter} target={blank} rel={external} aria-label="Twitter">
             <li>
               285 Tweets <Icons.Twitter size="20" />
             </li>
@@ -88,21 +65,21 @@ export default function About() {
         </Technologies>
       </Wrapper>
     </>
-  );
+  )
 }
 
 const Wrapper = styled.div`
   font-family: "Teko", sans-serif;
   letter-spacing: 1px;
   font-size: 1.4em;
-`;
+`
 
 const AboutDescription = styled.main`
   padding: 0 20px;
   line-height: 1.5em;
   font-size: 1.1em;
   text-align: center;
-`;
+`
 
 const Rating = styled.ul`
   padding: 20px 0 10px 20px;
@@ -114,11 +91,12 @@ const Rating = styled.ul`
     background-color: #304d5b;
     color: #fff;
     font-weight: 300;
+    transition: all 0.2s ease;
   }
   a li:hover {
-    background-color: #ff5722;
+    background-color: #ff1493;
   }
-`;
+`
 
 const ListTitle = styled.li`
   background-color: #fff !important;
@@ -129,7 +107,7 @@ const ListTitle = styled.li`
   padding: 1px !important;
   font-weight: normal !important;
   letter-spacing: 0 !important;
-`;
+`
 
 const Services = styled.ul`
   padding: 10px 0 10px 20px;
@@ -145,7 +123,7 @@ const Services = styled.ul`
   li:hover {
     cursor: default;
   }
-`;
+`
 
 const Technologies = styled.ul`
   padding: 10px 0 10px 20px;
@@ -161,7 +139,7 @@ const Technologies = styled.ul`
   li:hover {
     cursor: default;
   }
-`;
+`
 
 const Stats = styled.ul`
   padding: 10px 0 10px 20px;
@@ -173,8 +151,9 @@ const Stats = styled.ul`
     background-color: #304d5b;
     color: #fff;
     font-weight: 300;
+    transition: all 0.2s ease;
   }
   a li:hover {
-    background-color: #ff5722;
+    background-color: #ff1493;
   }
-`;
+`
