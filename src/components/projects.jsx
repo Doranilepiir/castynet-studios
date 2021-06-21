b3bbe680961b137f54244451ed0a0ed4befedfb8
castynet <riungemaina@gmail.com>
2021-06-21 18:47:01 +0300

    components/projects: changed to use pink

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,10 +1,10 @@
-import React from "react";
-import styled from "styled-components";
-import { ProjectsData } from "./utilities";
-import { Icons } from "./utilities";
+import React from "react"
+import styled from "styled-components"
+import { ProjectsData } from "./utilities"
+import { Icons } from "./utilities"
 
 export default function Projects() {
-  const projects = ProjectsData.items;
+  const projects = ProjectsData.items
 
   return (
     <>
@@ -17,23 +17,24 @@ export default function Projects() {
               {projects.type}
             </Type>
             <Button as="a" href={projects.link}>
-              View Project <Icons.Link size="30" />
+              View Project<span className="no-underline"></span>
+              <Icons.Link size="15" />
             </Button>
           </ProjectWrapper>
         ))}
       </Wrapper>
     </>
-  );
+  )
 }
 
 const Image = styled.img`
   width: 100%;
-`;
+`
 
 const Badge = styled(Icons.Check)`
   color: #00a700;
   padding: 0 10px 0 0;
-`;
+`
 
 const ProjectWrapper = styled.div`
   background-color: #fff;
@@ -41,11 +42,12 @@ const ProjectWrapper = styled.div`
   padding: 10px;
   border-radius: 2px;
   margin: 10px;
+  transition: all 0.4s ease;
 
   &:hover {
-    box-shadow: 0 0 8px 0 rgba(0, 0, 0, 0.98), 0 2px 10px 0 rgba(0, 0, 0, 0.98);
+    box-shadow: 0 0 8px 0 rgba(0, 0, 0, 0.38), 0 2px 10px 0 rgba(0, 0, 0, 0.38);
   }
-`;
+`
 
 const Wrapper = styled.div`
   display: flex;
@@ -54,20 +56,26 @@ const Wrapper = styled.div`
   align-content: center;
   grid-auto-flow: column;
   padding: 2.5em;
-  background-color: rgb(0 0 0 / 0.8);
-`;
+  background-color: rgb(0 0 0 / 0.45);
+`
 
 const Button = styled.button`
   text-align: center;
   margin: 0 0 1em 0;
-  text-transform: uppercase;
+  text-transform: lowercase;
   font-size: 0.9em;
-  color: #dd3500;
+  color: #1f2762;
+  font-family: "Roboto Mono", monospace;
+  transition: all 0.2s ease;
+
+  .no-underline {
+    padding-left: 7px;
+  }
 
   &:before {
     content: "";
     width: 100px;
-    background-color: #0176d3;
+    background-color: #ff1493;
     height: 8px;
     position: absolute;
     margin-top: 10px;
@@ -77,8 +85,14 @@ const Button = styled.button`
   &:hover:before {
     opacity: 0.3;
   }
-`;
+
+  &:hover {
+    color: #ff1493;
+  }
+`
 
 const Type = styled.p`
-  margin: 0.5em 0 1em 0;
-`;
+  margin: 0.5em 0 0.3em 0;
+  font-family: "Teko", sans-serif;
+  font-size: 1.4em;
+`
