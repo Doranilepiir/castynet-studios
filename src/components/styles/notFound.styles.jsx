84389795dc6888e8618ad667ed4e758427af6ae0
castynet <riungemaina@gmail.com>
2021-06-24 21:31:25 +0300

    components/styles/notFound.styles: created and added styles

---
+++ src/components/styles/notFound.styles.jsx
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  text-align: center;
+  margin: 0;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  line-height: 25px;
+  font-size: 1.5em;
+  font-family: "Teko", sans-serif;
+  letter-spacing: 0.5px;
+`;
+
+export const IconsWrap = styled.div`
+  display: grid;
+  grid-auto-flow: column;
+  justify-content: space-between;
+  align-content: center;
+  gap: 15px;
+  max-width: 200px;
+  margin: 5px auto 0 auto;
+  border-top: 1px solid #1f2762;
+  padding: 5px 20px 0 20px;
+
+  a {
+    color: #1f2762;
+
+    &:hover {
+      color: #ff69b4;
+    }
+  }
+`;
